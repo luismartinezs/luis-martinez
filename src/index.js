5f1b0ab8298cccc50ef4ab36957da65611eb2518
Luis Martinez <martinez.s.luis@gmail.com>
2019-05-29 21:58:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export const getTimeSinceStartingCoding = function getTimeSinceStartingCoding() {
+  const startingDate = new Date('2018-09-01');
+  const now = Date.now();
+  return {
+    years: 1,
+    months: 8
+  };
+};
---
+++ src/index.spec.js
@@ -0,0 +1,3 @@
+import { getTimeSinceStartingCoding } from './index';
+
+describe('getTimeSinceStartingCoding', );
