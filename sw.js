8f47b74cdd3096afc65a47d82a3de79bc97675ce
Luis Martinez <martinez.s.luis@gmail.com>
2019-06-09 21:10:32 +0200

    added service worker
    - Now the site works offline if it was cached

---
+++ sw.js
@@ -0,0 +1,19 @@
+// importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function initServiceWorker(e) {
+  e.waitUntil(
+    caches.open('luis-martinez-site').then(function cacheResources(cache) {
+      return cache.addAll(['/index.html', '/src/style.css']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function fetchResource(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function getResponse(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
